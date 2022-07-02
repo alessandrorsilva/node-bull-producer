9bf5ee4b25fc937eeec00f674db8e297cb1a8776
Alessandro Rodrigues da Silva <alessandrosilva.ti@gmail.com>
2022-07-02 14:51:55 -0300

    aula 2 - 02

--- src/api/index.ts
+++ src/api/index.ts
@@ -19,8 +19,20 @@ const postEmail = async (req, res) => {
   return res.send(body);
 };
 
+const postCandidate = async (req, res) => {
+  const body: { 
+    partyNumber: Number,
+    name:  string,
+    photo: string,
+  } = req.body;
+  await queues.candidate.add(body);
+  return res.send(body).status(200);
+};
+
+router.get('/', getPing);
 router.post('/log', postLog);
 router.post('/email', postEmail);
-router.get('/', getPing);
+router.post('/candidate', postCandidate);
+
 
 export default router;
